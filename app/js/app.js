351a0021c8e409e4a0957082d87141179bace84b
jingniwei <jingniwei@gmail.com>
2015-02-16 14:53:27 -0500

    first commit with some setup

---
+++ app/js/app.js
@@ -0,0 +1,43 @@
+/*global angular, ngRoute*/
+var app = angular.module('app', ['ngRoute']);
+
+ 
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/main', {
+        templateUrl: 'views/main.html',
+        controller: 'MainController'
+      }).
+      when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutController'
+    }).
+      when('/blog', {
+        templateUrl: 'views/blog.html',
+        controller: 'BlogController'
+      }).
+      otherwise({
+        redirectTo: '/main'
+      });
+}]);
+ 
+ 
+app.controller('MainController', function($scope) {
+     console.log('hi main');
+    $scope.message = 'This is Add new main screen';
+     
+});
+ 
+ 
+app.controller('AboutController', function($scope) {
+  console.log('hi about');
+    $scope.message = 'This is Show about screen';
+ 
+});
+
+app.controller('BlogController', function($scope) {
+     
+    $scope.message = 'This is Add new blog screen';
+     
+});
